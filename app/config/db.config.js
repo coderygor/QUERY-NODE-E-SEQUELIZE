e9861db930a65fdc13043ad387ccf4112750cb30
coderygor <ygorpereira@indecx.com.br>
2022-05-23 16:45:12 -0300

    refactor: send port and driver in database connect

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,15 +2,18 @@ require('dotenv').config();
 
 module.exports = {
     HOST: process.env.RDB_CONNECTION,
-    PORT: "",
+    PORT: "1434",
     USER: process.env.RDB_USER_NAME,
     PASSWORD: process.env.RDB_PASSWORD,
     DB: process.env.RDB_DATABASE,
     dialect: "mssql",
-    pool: {
+    "dialectOptions": {
         "options": {
-            "encrypt": true
-        },
+             "encrypt": true
+         },
+    driver: "tedious",
+    },
+    pool: {
         max: 5,
         min: 0,
         acquire: 30000,
